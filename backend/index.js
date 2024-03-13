38db3575fa85141529619c20c026d0b392f34642
Elena Tsoy <ltsoy1120@gmail.com>
2024-03-13 08:31:31 +0500

    changed domens name

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5000/",
-      "https://wedding-api-two.vercel.app/",
+      "https://wedding-dv.vercel.app/",
       "*",
       null
     ]
