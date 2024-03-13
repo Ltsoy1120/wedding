6cf9338680d078ba0a279f0dd7f83220269c9e48
Elena Tsoy <ltsoy1120@gmail.com>
2024-03-13 09:24:24 +0500

    fixed

--- backend/index.js
+++ backend/index.js
@@ -8,15 +8,15 @@ dotenv.config()
 const PORT = 5000
 
 const app = express()
-app.use(cors())
+// app.use(cors())
 
-// app.use(
-//   cors({
-//     origin: ["http://localhost:3000", "https://wedding-dv.vercel.app", "*"],
-//     methods: ["GET", "POST", "DELETE", "OPTIONS"],
-//     credentials: true
-//   })
-// )
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://wedding-dv.vercel.app", "*"],
+    methods: ["GET", "POST", "DELETE", "OPTIONS"],
+    credentials: true
+  })
+)
 
 app.use(express.json())
 app.use("/api/users", usersRouter)
