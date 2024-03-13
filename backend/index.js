416aa20e428643d2a0182e9fafd5d960a28487cf
Elena Tsoy <ltsoy1120@gmail.com>
2024-03-13 09:05:55 +0500

    cors

--- backend/index.js
+++ backend/index.js
@@ -2,47 +2,21 @@ import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 import cors from "cors"
-import usersRouter from "./src/routes/users.js"
+import usersRouter from "./routes/users.js"
 
 dotenv.config()
 const PORT = 5000
 
 const app = express()
-app.use(cors())
+// app.use(cors())
 
-// app.use(
-//   cors({
-//     origin: [
-//       "http://localhost:3000",
-//       "https://wedding-dv.vercel.app",
-//       "*",
-//       null
-//     ],
-//     methods: ["GET", "POST", "DELETE", "OPTIONS"],
-//     credentials: true
-//   })
-// )
-
-// app.use(function (req, res, next) {
-//   // Website you wish to allow to connect
-//   res.setHeader("Access-Control-Allow-Origin", "*")
-
-//   // Request methods you wish to allow
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-//   )
-
-//   // Request headers you wish to allow
-//   res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type")
-
-//   // Set to true if you need the website to include cookies in the requests sent
-//   // to the API (e.g. in case you use sessions)
-//   res.setHeader("Access-Control-Allow-Credentials", true)
-
-//   // Pass to next layer of middleware
-//   next()
-// })
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://wedding-dv.vercel.app", "*"],
+    methods: ["GET", "POST", "DELETE", "OPTIONS"],
+    credentials: true
+  })
+)
 
 app.use(express.json())
 app.use("/api/users", usersRouter)
