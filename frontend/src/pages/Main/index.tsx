3933456575d2e4ba4bbec12bf4aff2ce00aa1f1c
Elena Tsoy <ltsoy1120@gmail.com>
2024-03-24 13:26:56 +0500

    f

--- frontend/src/pages/Main/index.tsx
+++ frontend/src/pages/Main/index.tsx
@@ -8,10 +8,10 @@ const Main = () => {
         <img className="bg-top" src="/static/images/bg-top.png" alt="bg-top" />
         <img
           className="mask-top"
-          src="/static/images/mask-top.svg"
+          src="/static/images/mask-top.png"
           alt="mask-top"
         />
-        <img className="spark" src="/static/images/spark.svg" alt="spark" />
+        <img className="spark" src="/static/images/spark.png" alt="spark" />
       </div>
       <div className="main__context">
         <h1 className="main__title antarctic">
