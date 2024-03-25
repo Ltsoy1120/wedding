cafb8da225ce0de581ac9d89c9cb0e7077e79baa
Elena Tsoy <ltsoy1120@gmail.com>
2024-03-25 11:33:32 +0500

    bg

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
         />
         <img
           className="mask-bottom"
-          src="/static/images/mask-bottom.png"
+          src="/static/images/mask-bottom1.png"
           alt="mask-bottom"
         />
         <img
