bdf58472ad9dd27c7434862e9894fe47c1375d0f
Elena Tsoy <ltsoy1120@gmail.com>
2024-03-25 10:41:59 +0500

    photo

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,13 @@
 import React, { Suspense, lazy } from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
+import Loader from "./components/Loader"
 
 const LazyApp = lazy(() => import("./App"))
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
-  <Suspense fallback={<div>Loading...</div>}>
+  <Suspense fallback={<Loader />}>
     <LazyApp />
   </Suspense>
 )
